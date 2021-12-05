0c4bf8ba1fe08d51de6e4104c1b23ce7f1387dbf
Politenkov Daniil <politenkov01@mail.ru>
2021-12-05 23:59:15 +0800

    Изменения для 3 лабы (CRUD)

--- public/index.php
+++ public/index.php
@@ -28,6 +28,7 @@ $context = [];
 $controller = null;
 //$controller = new Controller404($twig);*/
 $pdo = new PDO("mysql:host=localhost;dbname=outer_space;charset=utf8", "root", "");
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $router = new Router($twig, $pdo);
 
